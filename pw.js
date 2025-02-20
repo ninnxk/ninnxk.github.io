318ff3b7d1700275faf2ab3933eb9d579243f048
ninnxk <yaninlin93@gmail.com>
2025-02-21 00:04:16 +0700

    ai

---
+++ pw.js
@@ -0,0 +1,8 @@
+function checkPassword() {
+    var password = document.getElementById("password").value;
+    var correctPassword = "ninonlyeiei"; 
+    
+    if (password === correctPassword) {
+        window.location.href = "eiei.html";
+    }
+}
